7caaa01f23efd0b6c2343da3ceeee6611ac56cce
Rutte Silva <rosrosrutte@gmail.com>
2022-04-01 22:10:30 -0300

    Finaliza o exercício

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int numeroDigitado;
+
+Console.Write("Digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+if (numeroDigitado < 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("Negativo");
+}
+else if (numeroDigitado > 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine("Positivo");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("Zero");
+}
+
+Console.ResetColor();
